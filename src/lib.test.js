b787f74582beb2f85e729136609b73b5da918010
meyerhoff20042 <braden.meyerhoff@student.swic.edu>
2024-05-04 21:52:03 -0500

    Sort tests in alphabetical order

--- src/lib.test.js
+++ src/lib.test.js
@@ -6,37 +6,6 @@ import {
   merge2ArraysIntoAnArrayOfObjects,
 } from "./lib";
 
-test("removeCorrespondingItemsByTerm", () => {
-  // Arrange
-  const terms1 = ["term1", "term2", "term3"];
-  const terms2 = ["corresponding1", "corresponding2", "corresponding3"];
-  const filterTerm = "term2";
-
-  const expected = {
-    terms1: ["term1", "term3"],
-    terms2: ["corresponding1", "corresponding3"],
-  };
-
-  // Act
-  const result = removeCorrespondingItemsByTerm({ terms1, terms2, filterTerm });
-
-  // Assert - Check if the function correctly removes the corresponding items
-  expect(result.terms1).toEqual(expected.terms1);
-  expect(result.terms2).toEqual(expected.terms2);
-});
-
-test("sortByFirstName", () => {
-  // Arrange
-  const args = {
-    elements: ["John Doe", "Jane Smith", "Ralph Johnson"],
-    sortBy: "first",
-  };
-
-  const sortedElements = sortByName(args);
-
-  expect(sortedElements).toEqual(["Jane Smith", "John Doe", "Ralph Johnson"]);
-});
-
 describe("getLastName", () => {
   test("returns the last name from a full name", () => {
     // Arrange
@@ -109,4 +78,35 @@ describe("getLastName", () => {
     // Assert
     expect(result).toBe(expected);
   });
+});
+
+test("removeCorrespondingItemsByTerm", () => {
+  // Arrange
+  const terms1 = ["term1", "term2", "term3"];
+  const terms2 = ["corresponding1", "corresponding2", "corresponding3"];
+  const filterTerm = "term2";
+
+  const expected = {
+    terms1: ["term1", "term3"],
+    terms2: ["corresponding1", "corresponding3"],
+  };
+
+  // Act
+  const result = removeCorrespondingItemsByTerm({ terms1, terms2, filterTerm });
+
+  // Assert - Check if the function correctly removes the corresponding items
+  expect(result.terms1).toEqual(expected.terms1);
+  expect(result.terms2).toEqual(expected.terms2);
+});
+
+test("sortByFirstName", () => {
+  // Arrange
+  const args = {
+    elements: ["John Doe", "Jane Smith", "Ralph Johnson"],
+    sortBy: "first",
+  };
+
+  const sortedElements = sortByName(args);
+
+  expect(sortedElements).toEqual(["Jane Smith", "John Doe", "Ralph Johnson"]);
 });
